aed2e0692c3056869ec30db9aaed42cc3553a539
KimJaeGeun <jgk9612@gmail.com>
2022-11-10 11:26:42 +0900

    feat: Add ArticleDto to layer classification
    dto 계층과 entity 계층을 분리하기위해 dto 를 추가 정의

---
+++ src/main/java/com/mustache/board/domain/dto/ArticleDto.java
@@ -0,0 +1,26 @@
+package com.mustache.board.domain.dto;
+
+import com.mustache.board.domain.entity.Article;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class ArticleDto {
+    private Long id;
+    private String title;
+    private String contents;
+
+    public ArticleDto(String title, String contents) {
+        this.title = title;
+        this.contents = contents;
+    }
+
+    public Article toEntity() {
+        return new Article(title, contents);
+    }
+
+    public Article toEntity(Long id) {
+        return new Article(id, title, contents);
+    }
+}
