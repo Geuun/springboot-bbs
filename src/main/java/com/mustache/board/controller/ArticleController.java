a37b6ecee55143419669461e552257b8a01da88f
KimJaeGeun <jgk9612@gmail.com>
2022-11-10 13:08:23 +0900

    feat: Add New Article POST API
    POST API를 통해 repository로 저장

--- src/main/java/com/mustache/board/controller/ArticleController.java
+++ src/main/java/com/mustache/board/controller/ArticleController.java
@@ -1,8 +1,12 @@
 package com.mustache.board.controller;
 
+import com.mustache.board.domain.dto.ArticleDto;
+import com.mustache.board.domain.entity.Article;
+import com.mustache.board.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,8 +14,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/articles")
 public class ArticleController {
 
+    private final ArticleRepository articleRepository;
+
+    public ArticleController(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
     @GetMapping("/new")
     public String createArticlePage() {
         return "articles/new";
+    }
+
+    @PostMapping("/posts/")
+    public String createArticle(ArticleDto articleDto) {
+        log.info(articleDto.toString());
+        Article savedArticle = articleRepository.save(articleDto.toEntity());
+        log.info("generatedId: {}", savedArticle.getId());
+        return String.format("redirect:/articles/%d", savedArticle.getId());
     }
 }
