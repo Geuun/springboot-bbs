cc6d3061f1d11f1b164a8d0860c746575bcd6231
KimJaeGeun <jgk9612@gmail.com>
2022-11-11 14:02:50 +0900

    feat: Add Hospital Inquiry system Base API

---
+++ src/main/java/com/mustache/board/controller/HospitalController.java
@@ -0,0 +1,33 @@
+package com.mustache.board.controller;
+
+import com.mustache.board.domain.entity.Hospital;
+import com.mustache.board.repository.HospitalRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hospitals")
+@Slf4j
+public class HospitalController {
+    private final HospitalRepository hospitalRepository;
+
+    public HospitalController(HospitalRepository hospitalRepository) {
+        this.hospitalRepository = hospitalRepository;
+    }
+
+
+    @GetMapping("")
+    public String hospitalList(Model model, Pageable pageable) {
+        Page<Hospital> hospitals = hospitalRepository.findAll(pageable);
+        log.info("size: {}", hospitals.getSize());
+        model.addAttribute("hospitals", hospitals);
+        model.addAttribute("previous", pageable.previousOrFirst().getPageNumber());
+        model.addAttribute("next", pageable.next().getPageNumber());
+        return "hospitals/list";
+    }
+}
