a87574cb6661fe945455f5d7bf10436ab0fb871d
KimJaeGeun <jgk9612@gmail.com>
2022-11-21 01:20:01 +0900

    refactor: Modify deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
 
 # deploy.sh
 # 실행조건
-# 프로젝트 명 == 이미지 태그 == 컨테이너 이름 
+# deploy.sh 파일이 Project Root Directory에 위치
+# 프로젝트 명 == 이미지 태그 == 컨테이너 이름
 # url, username, password => 환경변수
 # sudo 권한 or docker 권한
 # {
@@ -13,36 +14,33 @@
 #     <port> : ${args[4]}
 #     <-d> : ${args[5]}
 # }
-# 입력예시 
+# 입력예시
 # -> springboot url root 1234 8080:8080 Y
-echo "***********************"
+
+# params 입력받기
+echo "**********************************************"
 echo "Please Enter The Params"
-echo "<ProjectName> <url> <username> <password> <port> <-d : Y/N>"
 read -p "ProjectName : "  ProjectName
-read -p "URL : " URL
-read -p "UserName : " UserName
-read -p "Password : " Password
+read -p "DB_URL : " DB_URL
+read -p "DB_UserName : " DB_UserName
+read -p "DB_Password : " DB_Password
 read -p "Port : " Port
 read -p "Daemon <Y/N> : " Daemon
-echo "***********************"
-
-# # 입력 받은 값 출력
-# echo "ProjectName: " ${args[0]}
-# echo "url: "${args[1]}
-# echo "username: "${args[2]}
-# echo "password: "${args[3]}
-# echo "port: "${args[4]}
-# echo ""${args[5]}
+echo "**********************************************"
 
 # 데몬 조건문
 if [ "${Daemon}" == "Y" ]
 then
         DemonOpt="-d"
-        echo "DemonOpt: ON"
-elif [ "${Daemon}" = "N" ]
+        echo "********************"
+        echo "*** DemonOpt: ON ***"
+        echo "********************"
+elif [ "${Daemon}" == "N" ]
 then
         DaemonOpt=""
-        echo "DemonOpt: OFF"
+        echo "*********************"
+        echo "*** DemonOpt: OFF ***"
+        echo "*********************"
 else
         exit 1
         echo "It's the wrong option Try Again"
@@ -51,10 +49,10 @@ fi
 
 # 최종 입력 값
 echo "*****************************************************************"
-echo "*** <ProjectName> <url> <username> <password> <port> <Deamon> ***"
+echo "*** <ProjectName> <DB_URL> <DB_UserName> <DB_Password> <Port> <Deamon> ***"
 echo "*****************************************************************"
 echo ""
-echo "Your params >>" ${ProjectName} ${URL} ${UserName} ${Password} ${Port}  ${DemonOpt}
+echo "Your params >>" ${ProjectName} ${DB_URL} ${DB_UserName} ${DB_Password} ${Port}  ${DemonOpt}
 echo ""
 
 # 기존 컨테이너 중지
@@ -68,11 +66,11 @@ docker stop ${ProjectName}
 docker rename ${ProjectName} old${ProjectName}
 
 
-# 프로젝트 폴더 진입 (절대경로)
-echo "**************************************"
-echo "*** Entering The Project Directory ***"
-echo "**************************************"
-cd ~/dev/${ProjectName}
+# # 프로젝트 폴더 진입 (절대경로)
+# echo "**************************************"
+# echo "*** Entering The Project Directory ***"
+# echo "**************************************"
+# cd ~/dev/${ProjectName}
 
 
 # git pull
@@ -83,22 +81,30 @@ git pull
 
 
 # 이미지 빌드
+ImageTag=${ProjectName}
 echo "*******************"
 echo "*** Build image ***"
 echo "*******************"
-docker build -t ${ProjectName} . 
+docker build -t ${ImageTag} .
 
 
 # 컨테이너 빌드 docker run [OPTIONS] IMAGE [COMMAND] [ARG...]
 echo "***********************"
 echo "*** Build Container ***"
 echo "***********************"
-docker run ${DemonOpt} --name ${ProjectName} -p ${Port} -e SPRING_DATASOURCE_URL=${URL} -e SPRING_DATASOURCE_USERNAME=${UserName} -e SPRING_DATASOURCE_PASSWORD=${Password} ${DaemonOpt}
+docker run ${DemonOpt} \
+--name ${ProjectName} \
+-p ${Port} \
+-e SPRING_DATASOURCE_URL=${DB_URL} \
+-e SPRING_DATASOURCE_USERNAME=${DB_UserName} \
+-e SPRING_DATASOURCE_PASSWORD=${DB_Password} \
+${ImageTag}
 
 # 완료 문장
 echo "##########################################################################################"
 echo "###################################  Deploy is Done! #####################################"
 echo "##########################################################################################"
+
 
 
 
