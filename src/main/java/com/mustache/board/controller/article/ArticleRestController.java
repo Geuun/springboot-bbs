de35ab6213406353c3a161007f6b476e25666c9c
KimJaeGeun <jgk9612@gmail.com>
2022-11-26 04:12:37 +0900

    refactor: Refactor dependent injects by using RequiredArgsConstructor

--- src/main/java/com/mustache/board/controller/article/ArticleRestController.java
+++ src/main/java/com/mustache/board/controller/article/ArticleRestController.java
@@ -4,20 +4,18 @@ import com.mustache.board.domain.article.dto.ArticleAddRequest;
 import com.mustache.board.domain.article.dto.ArticleAddResponse;
 import com.mustache.board.domain.article.dto.ArticleResponse;
 import com.mustache.board.service.ArticleService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/articles")
 public class ArticleRestController {
 
     private final ArticleService articleService;
-
-    public ArticleRestController(ArticleService articleService) {
-        this.articleService = articleService;
-    }
 
     @GetMapping("/{id}")
     public ResponseEntity<ArticleResponse> getArticle(@PathVariable Long id) {
