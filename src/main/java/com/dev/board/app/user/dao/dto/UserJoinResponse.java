a305820191a0ecd6c64dd065f62e86be1d553d30
KimJaeGeun <jgk9612@gmail.com>
2022-12-07 17:19:20 +0900

    feat: Add User Dao Layer
    1. 기본적인 User entity 작성
    2. 회원가입을 위한 dto 작성
    3. 모든 res 를 감싸서 반환할 Response 작성

---
+++ src/main/java/com/dev/board/app/user/dao/dto/UserJoinResponse.java
@@ -0,0 +1,15 @@
+package com.dev.board.app.user.dao.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserJoinResponse {
+    private String userName;
+    private String emailAddress;
+}
---
+++ src/main/java/com/dev/board/app/user/dao/entity/User.java
@@ -0,0 +1,25 @@
+package com.dev.board.app.user.dao.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String userName;
+    private String password;
+    private String emailAddress;
+}
+
